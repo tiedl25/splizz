b5b58940cc5d8ce432df8e6dfa4b1d719debed0d
tiedl25 <max.tiedl@gmx.de>
2024-11-16 11:52:55 +0100

    Add Bash scripts to pull and push the offline database

---
+++ pullDatabase.sh
@@ -0,0 +1,3 @@
+adb root
+adb pull /data/data/de.tmc.splizz/databases/my_repository.sqlite ./
+sqlitebrowser my_repository.sqlite
---
+++ pushDatabase.sh
@@ -0,0 +1,2 @@
+adb root
+adb push my_repository.sqlite /data/data/de.tmc.splizz/databases/
