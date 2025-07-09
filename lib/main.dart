17dd9fc3d8a1df81fa6ccdd8cf92a5124ab81893
tiedl25 <max.tiedl@gmx.de>
2025-07-09 17:28:19 +0200

    Add in-app update functionality

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:in_app_update/in_app_update.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:splizz/bloc/main_bloc.dart';
 import 'package:splizz/brick/repository.dart';
@@ -31,6 +32,27 @@ void main() async {
   if (sharedPreferences.getBool('offline') == null) {
     sharedPreferences.setBool('offline', false);
   }
+
+  InAppUpdate.checkForUpdate().then((updateInfo) {
+    if (updateInfo.updateAvailability == UpdateAvailability.updateAvailable) {
+        if (updateInfo.immediateUpdateAllowed) {
+            // Perform immediate update
+            InAppUpdate.performImmediateUpdate().then((appUpdateResult) {
+                if (appUpdateResult == AppUpdateResult.success) {
+                  //App Update successful
+                }
+            });
+        } else if (updateInfo.flexibleUpdateAllowed) {
+          //Perform flexible update
+          InAppUpdate.startFlexibleUpdate().then((appUpdateResult) {
+                if (appUpdateResult == AppUpdateResult.success) {
+                  //App Update successful
+                  InAppUpdate.completeFlexibleUpdate();
+                }
+            });
+        }
+    }
+  });
 
   runApp(MyApp(sharedPreferences: sharedPreferences));
 }
